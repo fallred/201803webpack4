d10d36dedf7e7d8028a8efe40bfe11f61374b946
fallred <fallred@sina.com>
2019-03-12 21:37:55 +0800

    html-loader 指定特定模板解析html

--- src/loaders/html-loader.js
+++ src/loaders/html-loader.js
@@ -1,12 +1,25 @@
 let {getOptions} = require('loader-utils');
 const fs = require('fs');
+const path = require('path');
 function loader(source) {
     // 把loader变成异步函数
     let cb = this.async();
     let { layout } = getOptions(this);
-    fs.readFile(layout, 'utf8', (err, data)=>{
-        source = data.replace('{{_content_}}', source);
-        cb(err, `module.exports = ${JSON.stringify(source)}`);
-    });
+    let reg = /@layout\((.+)\)/;
+    let result = source.match(reg);
+    if (result) {
+        let customLayout = result[1];
+        console.log('this.context', this.context); 
+        customLayout = path.resolve(this.context, customLayout);
+        fs.readFile(customLayout, 'utf8', (err, data)=>{
+            source = data.replace('{{_content_}}', source);
+            cb(err, `module.exports = ${JSON.stringify(source)}`);
+        });
+    } else {
+        fs.readFile(layout, 'utf8', (err, data)=>{
+            source = data.replace('{{_content_}}', source);
+            cb(err, `module.exports = ${JSON.stringify(source)}`);
+        });
+    }
 }
 module.exports = loader;
