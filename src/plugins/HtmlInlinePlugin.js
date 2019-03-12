51123ed51c3e7aa4413399360d0a0e64d8d65a90
fallred <fallred@sina.com>
2019-03-13 00:48:16 +0800

    HtmlInlinePlugin实现

--- src/plugins/HtmlInlinePlugin.js
+++ src/plugins/HtmlInlinePlugin.js
@@ -12,11 +12,40 @@ class HtmlInlinePlugin {
                  * { tagName: 'script',
                     closeTag: true,
                     attributes: { type: 'text/javascript', src: 'main.js' } }
+
+
+                    { tagName: 'link',
+                    closeTag: true,
+                    attributes: { rel: 'stylesheet', href: 'main.css' } }
                  */
-                let tag = htmlData.body[0];
-                tag.attributes.src='xxx.js';
-                htmlData.body = [tag];
-                console.log(util.inspect(htmlData.body[0]));
+                // let tag = htmlData.body[0];
+                // tag.attributes.src='xxx.js';
+                // htmlData.body = [tag];
+                // console.log(util.inspect(htmlData.body[0]));
+
+                htmlData.body = htmlData.body.map(tag=>{
+                    if (tag.tagName == 'script') {
+                        let newTag = {
+                            tagName: 'script',
+                            closeTag: true,
+                            attributes: {
+                                type: 'text/javascript'
+                            }
+                        }
+                        newTag.innerHTML = compilation.assets[tag.attributes.src].source();
+                        return newTag;
+                    } else if (tag.tagName == 'link') {
+                        let newTag = {
+                            tagName: 'style',
+                            closeTag: true,
+                            attributes: {
+                                type: 'text/css'
+                            }
+                        }
+                        newTag.innerHTML = compilation.assets[tag.attributes.href].source();
+                        return newTag;
+                    }
+                });
                 // 必须要调用回调函数
                 cb();
             });
